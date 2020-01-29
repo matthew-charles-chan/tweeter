729aaf87066b56bd938c9e8fa5de8ff2df37311c
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-29 05:21:06 +0000

    implementing new tweet functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,34 +16,28 @@ $(document).ready(function() {
       },
     "created_at": 1461116232227
  }
- createTweetElement = function(data) {
-   let $tweet = $("<article>").addClass("tweet")
-   
-   
-   
-   
-   
-   
-   
-   markup = `
-   <div class= "tweet">
-   <header class= "tweet-header">
-     <span><img src=${tweetData.user.avatars}></span>
-     <span class= "name">${tweetData.user.name}</span>
-     <span class= "handle">${tweetData.user.handle}/span>
-   </header>
-   <blockquote class="tweet-content"${tweetData.content.text}!</blockquote>
-   <hr class="ruler-line">
-   <footer class="tweet-footer">
-     <time>10 days ago</time>
-     <img src="images/heart-black.png">
-   </footer>
- </div>
-   `
+ let tweet = `
+  <div class= "tweet">
+    <header class= "tweet-header">
+      <span><img src=${tweetData.user.avatars}></span>
+      <span class= "name">${tweetData.user.name}</span>
+      <span class= "handle">${tweetData.user.handle}/span>
+    </header>
+    <blockquote class="tweet-content">${tweetData.content.text}!</blockquote>
+    <hr class="ruler-line">
+    <footer class="tweet-footer">
+      <time>10 days ago</time>
+      <img src="images/heart-black.png">
+    </footer>
+  </div>
+  `
+  const $input = $('input')
+  $input.on('click', (event) => {
+    const $tweet = $('#tweets-container')
+    const newTweet = $(tweet)
 
- }
+    $tweet.append(newTweet)
+  })
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
 });
