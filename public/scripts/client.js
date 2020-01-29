7daea045ef8c873e88760356fac03849f1e2d01a
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-29 20:40:35 +0000

    implemented tweet rendering

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,41 +5,55 @@
  */
 
 $(document).ready(function() {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
- }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
   // const createTweetElement = function (tweet) {
   //   let $tweet = $
   // }
 
-  // const $tweet = createTweetElement(tweetData) 
+  // const $tweet = createTweetElement(tweet) 
 
 
   const $input = $('input')
   $input.on('click', (event) => {
     const $tweet = $('<article>').addClass('tweet')
     let tweetElement = `
-      <header class= "tweet-header">
-        <div class = "creator-info">
-          <span><img src=${tweetData.user.avatars}></span>
-          <span class= "name">${tweetData.user.name}</span>
-        </div>
-        <span class= "handle">${tweetData.user.handle}</span>
-      </header>
-      <blockquote class="tweet-content">${tweetData.content.text}!</blockquote>
-      <hr class="ruler-line">
-      <footer class="tweet-footer">
-        <time>10 days ago</time>
-        <img src="images/heart-black.png">
-      </footer>
+    <header class= "tweet-header">
+      <img src = ${tweet.user.avatars}>
+      <span class= "name">${tweet.user.name}</span>
+      <span class= "handle">${tweet.user.handle}</span>
+    </header>
+
+    <blockquote class="tweet-content">${tweet.content.text}</blockquote>
+
+    <hr class="ruler-line">
+
+    <footer class="tweet-footer">
+      <time>10 days ago</time>
+      <img src="images/heart-black.png">
+    </footer>
       `
     $tweet.append(tweetElement);
 
@@ -47,29 +61,33 @@ $(document).ready(function() {
 
     // const renderTweets = function(tweet)
 
+    
 
-
-    const createTweetElement = function(tweet) {
-      const $tweet = $('<article>').addClass('tweet')
-      let tweetElement = `
-        <header class= "tweet-header">
-          <div class = "creator-info">
-            <span><img src=${tweetData.user.avatars}></span>
-            <span class= "name">${tweetData.user.name}</span>
-          </div>
-          <span class= "handle">${tweetData.user.handle}</span>
-        </header>
-        <blockquote class="tweet-content">${tweetData.content.text}!</blockquote>
-        <hr class="ruler-line">
-        <footer class="tweet-footer">
-          <time>10 days ago</time>
-          <img src="images/heart-black.png">
-        </footer>
-      `
-      $tweet.append(tweetElement);
-      return $tweet
-    }
   })
+  const renderTweets = function(tweets) {
+    $tweets = $("#tweets-container")
+    tweets.forEach( (tweet) => {
+      $tweets.append(createTweetElement(tweet))
+    })
+  }
 
-
+  const createTweetElement = function(tweet) {
+    const $tweet = $('<article>').addClass('tweet')
+    let tweetElement = `
+      <header class= "tweet-header">
+        <img src=${tweet.user.avatars}>
+        <span class= "name">${tweet.user.name}</span>
+        <span class= "handle">${tweet.user.handle}</span>
+      </header>
+      <blockquote class="tweet-content">${tweet.content.text}!</blockquote>
+      <hr class="ruler-line">
+      <footer class="tweet-footer">
+        <time>10 days ago</time>
+        <img src="images/heart-black.png">
+      </footer>
+    `
+    $tweet.append(tweetElement);
+    return $tweet
+  }
+  renderTweets(data);
 });
