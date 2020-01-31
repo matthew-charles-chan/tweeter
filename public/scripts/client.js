ee8f6ce3c77d2cd7a3d94f9087e5fad1c4ba1da2
matthew-charles-chan <matthew.charles.chan@gmail.com>
2020-01-31 03:32:51 +0000

    adding icons in tweet footer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -62,8 +62,14 @@ $(document).ready(function() {
 
     // adding class, values and attributes to FOOTER elements
     const $time = $('<time>').text(timeAgo);
-    const $heart = $('<img>').attr("src", "images/heart-black.png");
-    $footer.append($time, $heart);
+    
+    const $icons = $('<div>').addClass('icons');
+    const $heart = $('<svg width="24" height="24" viewBox="0 0 24 24" fill="black" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>')
+    const $flag = $('<svg width="24" height="24" viewBox="0 0 24 24" fill="black" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-flag"><path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path><line x1="4" y1="22" x2="4" y2="15"></line></svg>')
+    const $retweet = $('<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>')
+    
+    $icons.append($flag, $retweet, $heart)
+    $footer.append($time, $icons);
     
     // appends indavidual elements to article('tweet') element
     $tweet.append($header, $blockquote, $hr, $footer);
